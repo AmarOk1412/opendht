7445f1492c1e7e4d6bee11fec083059995348920
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-11-28 15:00:56 -0500

    dhtproxy: support push notifications for mobile clients
    + Add SUBSCRIBE and UNSUBSCRIBE for DhtProxyServer
    + Add the ability to stop Listen via a timeout (and send a push
    Notification for the client)
    + Add the ability to use gorush to send push notifications
    
    Note: Update dhtnode

--- src/dht_proxy_server.cpp
+++ src/dht_proxy_server.cpp
@@ -398,6 +398,10 @@ DhtProxyServer::sendPushNotification(const std::string& key, const Json::Value& 
     auto content = std::string("{\"notifications\": [{\"tokens\": [\""
     + key + "\"], \"platform\":  " + std::to_string(platform)
     + ",\"message\": \"" + valueStr + "\"}]}");
+<<<<<<< HEAD
+=======
+    std::cout << content << std::endl;
+>>>>>>> dhtproxy: support push notifications for mobile clients
     req->set_header("Content-Type", "application/json");
     req->set_header("Accept", "*/*");
     req->set_header("Host", pushServer_);
